636cb1a21ca7e50ce7be6313b41a6dd46b6c64d4
Seno @ ThinkPad <senoadiw@gmail.com>
2017-09-28 19:12:08 +0700

    tutorial #2

--- App.js
+++ App.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Header } from 'react-native-elements';
+import { View, TextInput } from 'react-native';
+import { Header, Button } from 'react-native-elements';
 
 export default class App extends Component {
+  state = { term: '' };
+
   render() {
+    const {
+      containerStyle,
+      searchTextStyle,
+      buttonStyle
+    } = styles;
+
     return (
       <View style={{ flex: 1, backgroundColor: '#ddd' }}>
         <Header
           centerComponent={{ text: 'Simple Youtube Search', style: { color: '#fff' } }}
           outerContainerStyles={{ backgroundColor: '#e62117' }}
         />
-        {/*searchbar*/}
+        <View style={containerStyle}>
+          <TextInput
+            style={searchTextStyle}
+            onChangeText={term => this.setState({ term: term })}
+            value={this.state.term}
+          />
+          <Button
+            buttonStyle={buttonStyle}
+            title="Search"
+            onPress={() => console.log(this.state.term)}
+          />
+        </View>
         {/*videolist*/}
       </View>
     );
+  }
+}
+
+const styles = {
+  containerStyle: {
+    marginTop: 75,
+    marginLeft: 10,
+    marginRight: 10,
+    flexDirection: 'row'
+  },
+  searchTextStyle: {
+    flex: 1
+  },
+  buttonStyle: {
+    height: 30,
+    marginBottom: 8
   }
 }
