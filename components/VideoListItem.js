e06ac0899fd8dad0b704a6c9d641a327d36f48e9
Seno @ ThinkPad <senoadiw@gmail.com>
2017-09-29 18:26:47 +0700

    tutorial #7

--- components/VideoListItem.js
+++ components/VideoListItem.js
@@ -1,24 +1,56 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
+import { Card } from 'react-native-elements';
 
 const VideoListItem = ({ video }) => {
     return (
         <View>
-            <Image
-                style={styles.imageStyle}
-                source={{ uri: video.snippet.thumbnails.medium.url }}
-            />
-            <Text>{video.snippet.title}</Text>
-            <Text>{video.snippet.channelTitle}</Text>
-            <Text>{video.snippet.description}</Text>
+            <Card containerStyle={styles.cardStyle}>
+                <Image
+                    style={styles.imageStyle}
+                    source={{ uri: video.snippet.thumbnails.medium.url }}
+                />
+                <View style={styles.contentStyle}>
+                    <Text style={styles.titleStyle}>
+                        {video.snippet.title}
+                    </Text>
+                    <Text style={styles.channelTitleStyle}>
+                        {video.snippet.channelTitle}
+                    </Text>
+                    <Text style={styles.descriptionStyle}>
+                        {video.snippet.description}
+                    </Text>
+                </View>
+            </Card>
         </View>
     );
 };
 
 const styles = {
+    cardStyle: {
+        padding: 5
+    },
     imageStyle: {
         alignSelf: 'stretch',
         height: 180
+    },
+    contentStyle: {
+        flex: 1,
+        padding: 5
+    },
+    titleStyle: {
+        fontSize: 12,
+        marginBottom: 5
+    },
+    channelTitleStyle: {
+        fontSize: 11,
+        color: '#777',
+        marginBottom: 5,
+        alignSelf: 'flex-end'
+    },
+    descriptionStyle: {
+        fontSize: 11,
+        alignSelf: 'center'
     }
 }
 
