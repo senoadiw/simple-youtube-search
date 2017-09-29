882ed17e8d3d766d478f2887ec0fd2be3cd7221e
Seno @ ThinkPad <senoadiw@gmail.com>
2017-09-29 14:45:47 +0700

    tutorial #4

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { View } from 'react-native';
 import AppHeader from './components/AppHeader';
 import SearchBar from './components/SearchBar';
 
+const API_KEY = 'AIzaSyCkYVWVBpcDINDrEOiwJl6cfi4sbyKbo-w';
+
 export default class App extends Component {
   onPressSearch = term => {
     console.log(term);
